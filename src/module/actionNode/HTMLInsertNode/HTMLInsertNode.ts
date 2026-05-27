9c13eebd09c8afa125c7c181eea6167c3c2cb709
Ricky <rckruse91@gmail.com>
2026-05-26 21:45:20 -0400

    Draw pixi rectangles over highlighted elements for HTMLInsert SWAN

--- src/module/actionNode/HTMLInsertNode/HTMLInsertNode.ts
+++ src/module/actionNode/HTMLInsertNode/HTMLInsertNode.ts
@@ -1,7 +1,11 @@
+import { ICanvas } from "pixi.js";
 import { SWAN } from "../base";
 
 class HTMLInsertNode extends SWAN {
     document: HTMLElement;
+    declare pixiApp: PIXI.Application;
+    pixiGraphics: PIXI.Graphics = new PIXI.Graphics();
+
     constructor(document: HTMLElement) {
         super();
 
@@ -9,21 +13,37 @@ class HTMLInsertNode extends SWAN {
     }
 
     targetHTMLElement() {
-        let originalStyle;
-        this.document.addEventListener("mouseover", (event: Event) => {
-            console.log(event.target);
-            if (event.target instanceof HTMLElement) {
-                originalStyle = String(event.target.style);
-                event.target.style = "border: 5px solid red;";
-            }
-        });
+        this.document.innerHTML += `<canvas style="position: absolute; top: 0; left: 0; pointer-events: none; width: 100%; height: 100%; z-index: 100;"></canvas>`;
 
-        this.document.addEventListener("mouseout", (event: Event) => {
-            if (event.target instanceof HTMLElement) {
-                originalStyle = String(event.target.style);
-                event.target.style = originalStyle;
+        const canvas = this.document.querySelector("canvas");
+        if (canvas) {
+            this.pixiApp = new PIXI.Application({
+                view: canvas as ICanvas,
+                width: this.document.clientWidth,
+                height: this.document.clientHeight,
+                backgroundAlpha: 0,
+            });
+
+            this.pixiApp.stage.addChild(this.pixiGraphics);
+        }
+
+        this.document.addEventListener("mouseover", (event: Event) => {
+            const hoverTarget = event.target;
+            if (hoverTarget instanceof HTMLElement) {
+                this.updatePixiRect(
+                    hoverTarget.offsetLeft,
+                    hoverTarget.offsetTop,
+                    hoverTarget.clientWidth,
+                    hoverTarget.clientHeight,
+                );
             }
         });
+    }
+
+    updatePixiRect(x: number, y: number, w: number, h: number): void {
+        this.pixiGraphics.clear();
+        this.pixiGraphics.lineStyle(4, 0xff0000, 1);
+        this.pixiGraphics.drawRect(x, y, w, h);
     }
 }
 
